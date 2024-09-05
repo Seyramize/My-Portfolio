88205d28f91ae4b69c23828e5d6ef8d3f4797168
Seyramize <seyramalifo@gmail.com>
2024-09-05 07:39:00 -0700

    Changed dummy data from testimonials

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -1,8 +1,7 @@
-import memojiAvatar1 from "@/assets/images/memoji-avatar-1.png";
-import memojiAvatar2 from "@/assets/images/memoji-avatar-2.png";
-import memojiAvatar3 from "@/assets/images/memoji-avatar-3.png";
-import memojiAvatar4 from "@/assets/images/memoji-avatar-4.png";
-import memojiAvatar5 from "@/assets/images/memoji-avatar-5.png";
+import memojiAvatar1 from "@/assets/images/asabir.png";
+import memojiAvatar2 from "@/assets/images/charles.png";
+import memojiAvatar3 from "@/assets/images/roger.png";
+import memojiAvatar4 from "@/assets/images/jpm.png";
 import { SectionHeader } from "@/components/SectionHeader";
 import Image from "next/image";
 import { Card } from "@/components/Card";
@@ -12,32 +11,26 @@ const testimonials = [
   {
     name: "Mr. Asabir",
     position: "Chief Executive Officer @ TAAM",
-    text: "Alex was instrumental in transforming our website into a powerful marketing tool. His attention to detail and ability to understand our brand is exceptional. We're thrilled with the results!",
+    text: "Mr.Alifo played a pivotal role in establishing our organization's network infrastructure, turning it into a robust foundation for our operations. His meticulous approach and deep understanding of our specific needs were exceptional. We are extremely satisfied with the outcome!",
     avatar: memojiAvatar1,
   },
   {
-    name: "Olivia Green",
-    position: "Head of Design @ GreenLeaf",
-    text: "Working with Alex was a pleasure. His expertise in frontend development brought our designs to life in a way we never imagined. The website has exceeded our expectations.",
+    name: "Ing. Charles",
+    position: "Chief Executive Officer @ ALTO Systems",
+    text: "Working with Seyram was a pleasure. His expertise in mechanical, electrical, and plumbing systems brought our project to life in ways we hadn’t envisioned. The overall execution and quality of work exceeded our expectations.",
     avatar: memojiAvatar2,
   },
   {
-    name: "Daniel White",
-    position: "CEO @ InnovateCo",
-    text: "Alex's ability to create seamless user experiences is unmatched. Our website has seen a significant increase in conversions since launching the new design. We couldn't be happier.",
+    name: "Ing. Roger",
+    position: "Low Current Engineer @ UESD",
+    text: "Seyram's ability to design efficient and reliable electrical systems is unmatched. Since implementing his work, our operations have seen a significant improvement in performance and efficiency. We couldn't be more satisfied.",
     avatar: memojiAvatar3,
   },
   {
-    name: "Emily Carter",
-    position: "Product Manager @ GlobalTech",
-    text: "Alex is a true frontend wizard. He took our complex product and transformed it into an intuitive and engaging user interface. We're already seeing positive feedback from our customers.",
+    name: "Mr. John",
+    position: "Founder @ NYCJPM",
+    text: "Seyram is a true software and app development wizard. His insight on how he details how he worked on a complex product and transformed it into an intuitive and engaging application. We're already seeing positive feedback from our users.",
     avatar: memojiAvatar4,
-  },
-  {
-    name: "Michael Brown",
-    position: "Director of IT @ MegaCorp",
-    text: "Alex's work on our website has been nothing short of exceptional. He's a talented developer who is also a great communicator. We highly recommend him.",
-    avatar: memojiAvatar5,
   },
 ];
 
